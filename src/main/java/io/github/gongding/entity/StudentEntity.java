f5b3cb3621dd72564a72021b753440a90ea4b856
God_of_Admin <O0ohasho0O@proton.me>
2025-05-15 22:29:53 +0800

    feat(entity):实现学生实体类

---
+++ src/main/java/io/github/gongding/entity/StudentEntity.java
@@ -0,0 +1,96 @@
+package io.github.gongding.entity;
+
+import java.time.LocalDateTime;
+
+public class StudentEntity {
+    private int id;
+    private String studentNumber;
+    private String name;
+    private String email;
+    private String school;
+    private String classof;
+    private String passwordSalt;
+    private String passwordHash;
+    private LocalDateTime lastLogin;
+    private LocalDateTime createdAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getClassof() {
+        return classof;
+    }
+
+    public void setClassof(String classof) {
+        this.classof = classof;
+    }
+
+    public String getPasswordSalt() {
+        return passwordSalt;
+    }
+
+    public void setPasswordSalt(String passwordSalt) {
+        this.passwordSalt = passwordSalt;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public LocalDateTime getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(LocalDateTime lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
