8b3a58b98cab688c94bdf70a9afe91950741d51e
God_of_Admin <O0ohasho0O@proton.me>
2025-05-16 18:18:16 +0800

    feat(entity):实现课程实体类

---
+++ src/main/java/io/github/gongding/entity/LessonEntity.java
@@ -0,0 +1,60 @@
+package io.github.gongding.entity;
+
+import java.time.LocalDateTime;
+
+public class LessonEntity {
+    private int id;
+    private int teacherId;
+    private int semesterId;
+    private String title;
+    private String description;
+    private LocalDateTime createdAt;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(int teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public int getSemesterId() {
+        return semesterId;
+    }
+
+    public void setSemesterId(int semesterId) {
+        this.semesterId = semesterId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
